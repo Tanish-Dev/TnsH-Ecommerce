6d7d3bf6b824383cbfb3a47e1192964b6a51f099
Tanish <tanishbhavsar2007@gmail.com>
2025-06-10 17:36:32 +0530

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { cart } from "../data/cart.js";
+import "../data/backend-practice.js";
 
 console.log("Checkout page loaded with cart:", cart);
 
